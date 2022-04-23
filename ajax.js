8621c37ed6322592e1b1b2326035981d377a5e19
Thushara051989 <102418353+Thushara051989@users.noreply.github.com>
2022-04-24 00:10:00 +0530

    todo

--- ajax.js
+++ ajax.js
@@ -6,91 +6,61 @@ function loadajax() {
         var output="";
         for(var i=0;i<response.length;i++){
             if(response[i].completed==true){
-          output+= '<input type="checkbox" id="check" checked name="title" value=" <br>+  response[i].title  ; +" >' + response[i].title + '<br></input>';
+          output+= '<input type="checkbox" class="check" checked name="title" value=" <br>+  response[i].title  ; +" >' + response[i].title + '<br></input>';
             }
             else{
-                output+= '<input type="checkbox" id="check"  name="checkbox" value=" <br>+  response[i].title  ; +" >' + response[i].title + '<br></input>';  
+                output+= '<input type="checkbox" class="check"  name="checkbox" value=" <br>+  response[i].title  ; +" >' + response[i].title + '<br></input>';  
             }
+            
         }
+        
         document.getElementById("demo").innerHTML=output;
+        
+        
+      
+        
+        
+        
 
       }
-    };
+          };
+    
     xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
     
     xhttp.send();
     
     
   }
+
+  
   
 
-  // var promise=new Promise(function(resolve,reject){
-  //   window.onload=function(){
-  //     setTimeout(loadAfterTime,300);
-  //   };
-  //   function loadAfterTime(){
-  //     var inputElems=document.querySelectorAll("#checkbox");
-  //     // console.log(elem);
-  //     if(inputElems)
-  //       {
-  //          console.log(inputElems);
-
-  //       }
-  //       else{
-  //           console.log("empty");
-  //       }     
-  //       var count = 0;
-  //           inputElems.forEach(function(checkbox) {
-  //               checkbox.addEventListener('click', function() {
-  //                   if (this.checked) {
-  //                       console.log("Checkbox is checked..");
-  //                       count++;
-  //                   } else {
-  //                       count--;
-  //                       console.log("Checkbox is not checked..");
-  //                   }
-  //                   console.log(count);
-  //                   if(count==5)
-  //                   {
-  //                       resolve("Congrats. 5 Tasks have been Successfully Completed");
-  //                   }
-
-
-  //               });
-  //           });
-
-  //           promise.then(
-  //               function(result) { alert(result);},
-  //             );
-
-
-
-
+    // Alert for 5 check boxes
+    var demo=document.getElementById("demo");
+    var promise = new Promise(function(resolve,reject){
+       setTimeout(loadAfterTime, 3000);
+      
+       function loadAfterTime(){
+        var demo=document.getElementById("demo");
+        
+        demo.addEventListener('change', function() {
+      if(document.querySelectorAll('input[type="checkbox"]:checked').length==95){
+        resolve("Congrats. 5 Tasks have been Successfully Completed");
+        
+      }
+    }
+        )
+       };
+      
+      }
+        
+      ) ; 
+    promise.then(
+      function(result) { alert(result);},
+    );
     
-  //   }
-  // })
-//   function validate(){
-//     var checked=0;
-//     var chks=document.getElementsByTagName("input");
-//      console.log(chks.length);
-//     for (var k = 0; k < 200; k++) {
-//         if (chks[k].checked) {
-//             checked++;
-//         }
-//     }
-//     console.log(checked);
-//     if (checked==5){
-//         alert(checked + " CheckBoxe(s) are checked.");
-//     }
-// }
-// validate();
-// //     new Promise(function(resolve,reject){
-// //         if (checked ==5){
-// //             resolve(" 5 checkboxes are selected ");
-//         }
-//     })
+    
+   
+ 
 
-// Promise
-// .then(function(s){
-//    alert(s);
-// })
+  
